5405ad951a1058dec1d2289fe5ccca73e793a773
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2022-10-15 22:19:13 -0300

    implementação da lógica do WorkspaceService

--- goorg-java/src/main/java/com/goorg/goorgjava/service/WorkspaceService.java
+++ goorg-java/src/main/java/com/goorg/goorgjava/service/WorkspaceService.java
@@ -1,28 +1,32 @@
 package com.goorg.goorgjava.service;
 
 import com.goorg.goorgjava.model.workspace.Workspace;
+import com.goorg.goorgjava.repositories.WorkspaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
 public class WorkspaceService implements Service<Workspace, Long> {
+    @Autowired
+    private WorkspaceRepository repository;
     @Override
     public void save(Workspace workspace) {
-
+        this.repository.save(workspace);
     }
 
     @Override
     public void saveAll(List<Workspace> workspaces) {
-
+        this.saveAll(workspaces);
     }
 
     @Override
     public Iterable<Workspace> getAll() {
-        return null;
+        return this.repository.findAll();
     }
 
     @Override
     public Optional<Workspace> getById(Long aLong) {
-        return Optional.empty();
+        return this.repository.findById(aLong);
     }
 }
