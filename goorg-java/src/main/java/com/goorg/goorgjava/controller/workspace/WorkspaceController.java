2dd88a8f6099ef30ccd5030a1d897c722797cd74
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2022-10-18 11:49:44 -0300

    inicialização dos controllers de atividade e workspace

---
+++ goorg-java/src/main/java/com/goorg/goorgjava/controller/workspace/WorkspaceController.java
@@ -0,0 +1,37 @@
+package com.goorg.goorgjava.controller.workspace;
+
+import com.goorg.goorgjava.model.workspace.Workspace;
+import com.goorg.goorgjava.service.WorkspaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/workspace")
+public class WorkspaceController {
+    @Autowired
+    private WorkspaceService workspaceService;
+
+    @PostMapping
+    public @ResponseBody void postWorkspace(@Valid @RequestBody Workspace workspace){
+        this.workspaceService.save(workspace);
+    }
+
+    @PostMapping(path = "/all")
+    public @ResponseBody void postWorkspaces(@Valid @RequestBody List<Workspace> workspaces){
+        this.workspaceService.saveAll(workspaces);
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Workspace> getAllWorkspaces(){
+        return this.workspaceService.getAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Optional<Workspace> getWorkspacePorId(@PathVariable Long id){
+        return this.workspaceService.getById(id);
+    }
+}
--- goorg-java/src/main/java/com/goorg/goorgjava/service/AtividadeService.java
+++ goorg-java/src/main/java/com/goorg/goorgjava/service/AtividadeService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Optional;
 
+@org.springframework.stereotype.Service
 public class AtividadeService implements Service<Atividade,Long> {
     @Autowired
     private AtividadeRepository atividadeRepository;
