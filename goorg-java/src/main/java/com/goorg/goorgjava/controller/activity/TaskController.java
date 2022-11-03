a877a1ebbeb90d43260fdd657c3f5bfcc42906a4
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2022-11-03 08:22:38 -0300

    rename methods

--- goorg-java/src/main/java/com/goorg/goorgjava/controller/activity/TaskController.java
+++ goorg-java/src/main/java/com/goorg/goorgjava/controller/activity/TaskController.java
@@ -22,26 +22,26 @@ public class TaskController {
     private ActivityService activityService;
 
     @PostMapping(path = "/{activityId}")
-    public @ResponseBody void postActivity(@Valid @RequestBody Task task, @PathVariable Long activityId){
+    public @ResponseBody void postTask(@Valid @RequestBody Task task, @PathVariable Long activityId){
         Optional<Activity> activity = activityService.getById(activityId);
         task.setActivity(activity.orElseThrow( () -> new BadRequestException("Activity not found")));
         this.taskService.save(task);
     }
 
     @PutMapping(path = "/update/{activiyId}/{id}")
-    public @ResponseBody Task updateActivity(@PathVariable Long id, @PathVariable Long activiyId, @Valid @RequestBody Task task){
+    public @ResponseBody Task updateTask(@PathVariable Long id, @PathVariable Long activiyId, @Valid @RequestBody Task task){
         Optional<Activity> activity = this.activityService.getById(activiyId);
         task.setActivity(activity.orElseThrow( () -> new BadRequestException("Activity not found")));
         return this.taskService.update(id, task);
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Task> getAllActivities(){
+    public @ResponseBody Iterable<Task> getAllTasks(){
         return this.taskService.getAll();
     }
 
     @GetMapping(path = "/{id}")
-    public @ResponseBody Optional<Task> getActivityById(@PathVariable Long id){
+    public @ResponseBody Optional<Task> getTaskById(@PathVariable Long id){
         return this.taskService.getById(id);
     }
 }
